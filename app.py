5fd05d83b168bc255080e5e5abea7f474cf8ba9d
baru <pavbaru@gmail.com>
2025-10-01 16:22:07 +0200

    Vytvořena základní logika - během vytvoření zjištěn nedostatek - je potřeba užít cyklus while, aby aplikace nepadala po špatné volbě.

--- app.py
+++ app.py
@@ -5,3 +5,28 @@ from globalni import Globalni
 def main():
     evidence = Evidence()
     globalni = Globalni()
+
+print("*****************************")
+print("     Evidence pojištěnců     ")
+print("*****************************")
+print("Dostupné akce:               ")
+print("1 - Přidat nového pojištěnce ")
+print("2 - Vypsat všechny pojištěnce")
+print("3 - Vyhledat pojištěného     ")
+print("4 - Konec                    ")
+print("*****************************")
+print("Jakou operaci chcete vykonat?")
+akce=input()
+if (akce == "1"):
+    print("jede to")
+elif (akce == "2"):
+    print("jede to")
+elif (akce == "3"):
+    print("jede to")
+elif (akce == "4"):   
+    print("jede to")
+else: 
+    print("Neplatná operace")
+    print("Jakou operaci chcete vykonat?")
+    akce = input ()
+
